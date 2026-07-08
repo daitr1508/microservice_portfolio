44075b97b16a62abc3dfc67e98bb21aaaef709c7
dai99 <dai.nb@lpbs.com.vn>
2026-07-08 15:26:05 +0700

    Kafka

---
+++ usage-service/src/main/java/com/daidandy/kafka/event/AlertingEvent.java
@@ -0,0 +1,12 @@
+package com.daidandy.kafka.event;
+import lombok.Builder;
+
+@Builder
+public record AlertingEvent(
+        Long userId,
+        String message,
+        double threshold,
+        double energyConsumed,
+        String email
+) {
+}
---
+++ usage-service/src/main/java/com/daidandy/usage_service/dto/DeviceDto.java
@@ -0,0 +1,15 @@
+package com.daidandy.usage_service.dto;
+
+import lombok.Builder;
+import lombok.Setter;
+
+@Builder
+public record DeviceDto(
+        Long id,
+        String name,
+        String type,
+        String location,
+        Long userId,
+        Double energyConsumed
+) {
+}
